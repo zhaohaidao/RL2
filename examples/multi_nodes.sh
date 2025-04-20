de134dbbdf1f0d324c9e5542189ed97de9dc00e0
ChenmienTan <chenmientan@outlook.com>
2025-04-20 14:34:28 +0000

    fix typo for multi nodes

--- examples/multi_nodes.sh
+++ examples/multi_nodes.sh
@@ -3,7 +3,7 @@ export NCCL_IB_TIMEOUT=31
 
 torchrun \
     --nnodes=$WORLD_SIZE \
-    --nproc_per_node=$N_PROC_PER_NODE \
+    --nproc_per_node=$NPROC_PER_NODE \
     --rdzv_endpoint=$MASTER_ADDR:$MASTER_PORT \
     --rdzv-backend=c10d \
     --rdzv-conf=timeout=36000 \
