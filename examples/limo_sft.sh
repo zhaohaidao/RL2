b36f24214ee0c18aff1a677dfe97c5b6503507c8
ChenmienTan <chenmientan@outlook.com>
2025-05-16 12:53:28 +0000

    add example for SFT

---
+++ examples/limo_sft.sh
@@ -0,0 +1,13 @@
+torchrun \
+    --standalone \
+    --nnodes=1 \
+    --nproc_per_node=8 \
+    -m RL2.trainer.sft \
+    data.path=data/limo.json \
+    data.max_length=16384 \
+    data.batch_size=32 \
+    actor.model_name=Qwen/Qwen2.5-7B-Instruct \
+    actor.sp_size=2 \
+    trainer.project=LIMO \
+    trainer.experiment_name=qwen2.5-7b-inst \
+    trainer.n_epochs=15
