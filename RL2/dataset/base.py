7d7758dc825943e1c2ad01edd617ffc8d2c35e97
ChenmienTan <tanchenmien.tcme@alibaba-inc.com>
2025-05-23 11:52:24 +0000

    resolve circular import

--- RL2/dataset/base.py
+++ RL2/dataset/base.py
@@ -2,6 +2,33 @@ import json
 import torch
 from torch.utils.data import Dataset
 
+def tokenize_messages(tokenizer, messages):
+
+    states, actions, action_mask = [], [], []
+    for idx, message in enumerate(messages):
+
+        state = tokenizer.apply_chat_template(
+            messages[:idx + 1],
+            add_generation_prompt=idx + 1 < len(messages) and messages[idx + 1]["role"] == "assistant"
+        )[len(states):]
+
+        states.extend(state)
+        actions.extend(
+            state if message["role"] == "assistant"
+            else len(state) * [0]
+        )
+        action_mask.extend(len(state) * [
+            1 if message["role"] == "assistant" else 0
+        ])
+
+    return {
+        "states": states[:-1],
+        "actions": actions[1:],
+        "action_mask": action_mask[1:],
+        "position_ids": list(range(len(states) - 1))
+    }
+
+
 class BaseDataset(Dataset):
     
     def __init__(
