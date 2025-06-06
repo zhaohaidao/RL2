25c3ab8baf298e98971de1561f7e01253e425182
米晨 <tanchenmien.tcme@alibaba-inc.com>
2025-06-06 15:09:32 +0800

    improve DPO logic

--- RL2/dataset/dpo.py
+++ RL2/dataset/dpo.py
@@ -1,4 +1,3 @@
-import uuid
 import torch
 from RL2.dataset import BaseDataset, tokenize_messages
 
@@ -8,34 +7,25 @@ class DPODataset(BaseDataset):
     def __getitem__(self, idx):
 
         ex = self.dataset[idx]
-        uid = str(uuid.uuid4())
         messages = ex["messages"]
         chosen = ex["chosen"]
         rejected = ex["rejected"]
 
-        chosen = self.tokenize_messages_completion(
-            uid, messages, chosen, True
-        )
-        rejected = self.tokenize_messages_completion(
-            uid, messages, rejected, False
-        )
+        chosen = self.tokenize_messages_completion(messages, chosen)
+        rejected = self.tokenize_messages_completion(messages, rejected)
 
         return chosen, rejected
     
-    def tokenize_messages_completion(
-        self, uid, messages, completion, chosen: bool
-    ):
+    def tokenize_messages_completion(self, messages, completion):
 
         ex = tokenize_messages(
             self.tokenizer,
             messages + [{"role": "assistant", "content": completion}]
         )
         ex.update({
-            "uid": uid,
-            "eos_mask": torch.LongTensor((ex["states"].shape[-1] - 1) * [0] + [1]).unsqueeze(0),
-            "chosen_mask": torch.LongTensor(ex["states"].shape[-1] * [1 if chosen else -1]).unsqueeze(0)
+            "eos_mask": torch.LongTensor((ex["states"].shape[-1] - 1) * [0] + [1]).unsqueeze(0)
         })
-        return ex
+        return {k: v[:, :self.max_length] for k, v in ex.items()}
 
     def collate_fn(self, batch):
         return super().collate_fn(
