3216d7b4a78521d18cfe1123c8f359fa0e4fad5d
米晨 <tanchenmien.tcme@alibaba-inc.com>
2025-06-07 19:25:19 +0800

    separate config of ref actor

--- RL2/dataset/sft.py
+++ RL2/dataset/sft.py
@@ -7,3 +7,6 @@ class SFTDataset(BaseDataset):
         messages = self.dataset[idx]["messages"]
         ex = tokenize_messages(self.tokenizer, messages)
         return {k: v[:, :self.max_length] for k, v in ex.items()}
+    
+    def collate_fn(self, batch):
+        return list(batch)
