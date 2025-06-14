a505149f6e77a65ced021f4d6de7be05f49b739c
ChenmienTan <chenmientan@outlook.com>
2025-06-14 18:43:07 +0000

    seperate train and test max new tokens

--- examples/orz_reinforce.sh
+++ examples/orz_reinforce.sh
@@ -8,7 +8,8 @@ torchrun \
     actor.model_name=Qwen/Qwen2.5-7B \
     actor.max_length_per_device=16384 \
     actor.save_freq=32 \
-    actor.rollout.max_response_length=8192 \
+    actor.rollout.train_sampling_params.max_new_tokens=8192 \
+    actor.rollout.test_sampling_params.max_new_tokens=16384 \
     actor.rollout.env_path=envs/orz.py \
     adv.estimator=reinforce \
     trainer.project=OpenReasonerZero \
