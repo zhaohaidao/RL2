43d37725524af92682780f27c0a6b7ea9c10b1b3
ChenmienTan <chenmientan@outlook.com>
2025-06-14 04:07:49 +0000

    add SFT benchmark

---
+++ examples/limo_sft.sh
@@ -0,0 +1,12 @@
+torchrun \
+    --nproc_per_node=8 \
+    -m RL2.trainer.sft \
+    data.path=Chenmien/LIMO \
+    data.max_length=16384 \
+    data.batch_size=32 \
+    actor.model_name=Qwen/Qwen2.5-7B-Instruct \
+    actor.sp_size=2 \
+    actor.max_length_per_device=8192 \
+    trainer.project=LIMO \
+    trainer.experiment_name=qwen2.5-7b-inst \
+    trainer.n_epochs=15
