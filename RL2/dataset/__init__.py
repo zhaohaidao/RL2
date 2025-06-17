83394beae073abe170efc72393dfa1b6620c8e20
米晨 <tanchenmien.tcme@alibaba-inc.com>
2025-06-18 01:28:18 +0800

    support training RM

--- RL2/dataset/__init__.py
+++ RL2/dataset/__init__.py
@@ -1,4 +1,5 @@
 from .base import BaseDataset, tokenize_messages
 from .sft import SFTDataset
 from .dpo import DPODataset
+from .rm import RMDataset
 from .rl import RLDataset
---
+++ RL2/trainer/rm.py
@@ -0,0 +1,102 @@
+import hydra
+from collections import defaultdict
+import torch.nn.functional as F
+import torch.distributed as dist
+from transformers import (
+    AutoTokenizer,
+    AutoModelForSequenceClassification,
+    get_cosine_schedule_with_warmup
+)
+from tqdm import tqdm
+from RL2.trainer import Trainer
+from RL2.dataset import RMDataset
+from RL2.workers import Critic
+from RL2.algs import sequence_all_reduce
+from RL2.utils.comm import initialize_global_process_group
+from RL2.utils.timing import time_logger
+
+
+class RMTrainer(Trainer):
+
+    def __init__(self, config):
+        super().__init__(config)
+
+        tokenizer = AutoTokenizer.from_pretrained(config.critic.model_name)
+        dataset = RMDataset(
+            config.data.path, tokenizer, config.data.max_length
+        )
+        _, self.dataloader = self.prepare_sampler_dataloader(
+            dataset, self.config.data.batch_size, True
+        )
+
+        self.critic = Critic(config.critic, self.device_mesh)
+
+        num_training_steps = self.config.trainer.n_epochs * len(self.dataloader)
+        num_warmup_steps = int(self.config.critic.warmup_ratio * num_training_steps)
+        self.scheduler = get_cosine_schedule_with_warmup(
+            self.critic.optimizer,
+            num_warmup_steps=num_warmup_steps,
+            num_training_steps=num_training_steps
+        )
+
+    @time_logger("update_critic")
+    def update_critic(self, data_list, step):
+
+        minibatches = self.critic.scatter_and_pack_data_list(data_list, pair=True)
+
+        metrics = defaultdict(list)
+        for minibatch in self.critic.tqdm(minibatches):
+            rewards = self.critic.forward(minibatch)
+            chosen_rewards, rejected_rewards = sequence_all_reduce(
+                minibatch, rewards, self.critic.sp_device_mesh["sp"]
+            ).view(-1, 2).T
+            reward_margins = chosen_rewards - rejected_rewards
+            loss = - F.logsigmoid(reward_margins).sum() / self.config.data.batch_size
+            (loss * self.critic.device_mesh.size()).backward()
+
+            metrics["loss"].append(
+                self.critic.sp_device_mesh["dp"].size() * len(minibatches) * loss.item()
+            )
+            metrics["accuray"].extend((reward_margins > 0).tolist())
+
+        grad_norm = self.critic.optimizer_step()
+        self.scheduler.step()
+        metrics["grad_norm"].append(grad_norm)
+        self.critic.log(
+            metrics, step, self.critic.sp_device_mesh["dp"]
+        )
+
+    def train(self):
+
+        step = 0
+        for epoch in range(self.config.trainer.n_epochs):
+            for data_list in tqdm(
+                self.dataloader,
+                desc=f"Epoch {epoch + 1}",
+                disable=(self.device_mesh.get_rank() != 0)
+            ):
+                self.update_critic(data_list, step)
+                step += 1
+
+                if self.critic.config.save_freq is not None and step % self.critic.config.save_freq == 0:
+                    self.critic.save(
+                        step, model_cls=AutoModelForSequenceClassification
+                    )
+
+        self.critic.save(
+            step, model_cls=AutoModelForSequenceClassification
+        )
+
+
+@hydra.main(config_path="config", config_name="rm", version_base=None)
+def main(config):
+
+    initialize_global_process_group()
+
+    trainer = RMTrainer(config)
+    trainer.train()
+
+    dist.destroy_process_group()
+
+if __name__ == "__main__":
+    main()
