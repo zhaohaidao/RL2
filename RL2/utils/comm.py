a601fcd8314357d377566c4c6104ac8cfb54a4d5
ChenmienTan <chenmientan@outlook.com>
2025-06-18 04:10:04 +0000

    support model specific HSDP

--- RL2/utils/comm.py
+++ RL2/utils/comm.py
@@ -22,18 +22,23 @@ def gather_and_concat_list(lst, device_mesh=None):
     if device_mesh is None:
         world_size = dist.get_world_size()
         is_dst = dist.get_rank() == 0
+        dst = 0
         group = None
+        group_dst = None
     else:
         world_size = device_mesh.size()
         is_dst = device_mesh.get_local_rank() == 0
+        dst = None
         group = device_mesh.get_group()
+        group_dst= 0
     
     lists = [None] * world_size if is_dst else None
     dist.gather_object(
         lst,
         lists,
-        dst=0,
-        group=group
+        dst=dst,
+        group=group,
+        group_dst=group_dst
     )
 
     return sum(lists, []) if is_dst else None
