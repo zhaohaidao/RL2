9ff05211374aca17db48895dbf0767b89c29575c
ChenmienTan <chenmientan@outlook.com>
2025-06-18 13:18:07 +0000

    add example sktwork rm

---
+++ examples/skywork_rm.sh
@@ -0,0 +1,9 @@
+torchrun \
+    --nproc_per_node=4 \
+    -m RL2.trainer.rm \
+    data.path=Chenmien/SkyworkRM \
+    data.max_length=2048 \
+    critic.model_name=meta-llama/Llama-3.1-8B-Instruct \
+    critic.max_length_per_device=4096 \
+    trainer.project=SkyworkRM \
+    trainer.experiment_name=llama-3.1-8b-inst
