f6f4380f3284e83b639f437456032e2d875620c4
ChenmienTan <chenmientan@outlook.com>
2025-06-19 08:02:21 +0000

    disable logger of math_verify

--- envs/orz.py
+++ envs/orz.py
@@ -1,4 +1,8 @@
+import logging
 from math_verify import parse, verify
+
+logging.getLogger("math_verify.parser").disabled = True
+logging.getLogger("math_verify.grader").disabled = True
 
 def reward_fn(messages, answer):
     return verify(parse(answer), parse(messages[-1]["content"]))
