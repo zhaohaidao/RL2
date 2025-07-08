b9d333f77d9cdc349611be3738fbb9b9164f69cc
ChenmienTan <chenmientan@outlook.com>
2025-07-08 11:43:45 +0000

    update examples

--- examples/limo_sft.sh
+++ examples/limo_sft.sh
@@ -5,9 +5,8 @@ torchrun \
     data.max_length=16384 \
     data.batch_size=32 \
     actor.model_name=Qwen/Qwen2.5-7B-Instruct \
-    actor.use_liger_kernel=true \
-    actor.sp_size=2 \
-    actor.max_length_per_device=8192 \
+    actor.sp_size=4 \
+    actor.max_length_per_device=4096 \
     trainer.project=LIMO \
     trainer.experiment_name=qwen2.5-7b-inst \
     trainer.n_epochs=15
--- examples/orz_reinforce.sh
+++ examples/orz_reinforce.sh
@@ -6,8 +6,8 @@ torchrun \
     data.prompts_per_rollout=128 \
     data.responses_per_prompt=64 \
     actor.model_name=Qwen/Qwen2.5-7B \
-    actor.use_liger_kernel=true \
-    actor.max_length_per_device=16384 \
+    actor.sp_size=2 \
+    actor.max_length_per_device=8192 \
     actor.save_freq=32 \
     rollout.train_sampling_params.max_new_tokens=8192 \
     rollout.env_path=envs/orz.py \
